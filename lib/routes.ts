3b81eb006f9a910b84a52ee2a604df19ae75d027
Lydon <christopher.lydon@maritz.com>
2026-06-27 21:15:10 -0500

    Add Turn by Turn

--- lib/routes.ts
+++ lib/routes.ts
@@ -1,7 +1,15 @@
+export type RouteStep = {
+  instruction: string;
+  distance: number;
+  duration: number;
+  name: string;
+};
+
 export type RouteOption = {
   coords: number[][];
   duration: number;
   distance: number;
+  steps: RouteStep[];
 };
 
 export async function getRoutes(
@@ -11,7 +19,7 @@ export async function getRoutes(
   const url =
     `https://api.mapbox.com/directions/v5/mapbox/driving-traffic/` +
     `${from[0]},${from[1]};${to[0]},${to[1]}` +
-    `?alternatives=true&geometries=geojson&overview=full&access_token=${process.env.NEXT_PUBLIC_MAPBOX_TOKEN}`;
+    `?alternatives=true&geometries=geojson&overview=full&steps=true&access_token=${process.env.NEXT_PUBLIC_MAPBOX_TOKEN}`;
 
   const res = await fetch(url);
   const data = await res.json();
@@ -24,5 +32,11 @@ export async function getRoutes(
     coords: r.geometry.coordinates,
     duration: r.duration,
     distance: r.distance,
+    steps: r.legs?.[0]?.steps?.map((s: any) => ({
+      instruction: s.maneuver?.instruction || "Continue",
+      distance: s.distance,
+      duration: s.duration,
+      name: s.name || "",
+    })) || [],
   }));
 }
